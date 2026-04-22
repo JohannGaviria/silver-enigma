f88bc6c3db8aa70112efee36f87a8e88b1266b88
Johann Gaviria <johannmgr@gmail.com>
2026-04-22 17:57:52 -0500

    feat(shared): add domain layer base classes
    - Add BaseEntity
    - BaseDomainException
    - BaseValueObject
    
    Classes to establish the foundation for the domain layer.

---
+++ src/shared/domain/entities/base_entity.py
@@ -0,0 +1,34 @@
+"""This module contains the BaseEntity class."""
+
+from abc import ABC
+from dataclasses import dataclass
+from datetime import datetime
+from uuid import UUID
+
+
+@dataclass
+class BaseEntity(ABC):
+    """BaseEntity is a base class for entities in the domain layer.
+
+    Attributes:
+        id (UUID): The unique identifier of the entity.
+        created_at (datetime): The date and time when the entity was created.
+        update_at (datetime): The date and time when the entity was last updated.
+    """
+
+    id: UUID
+    created_at: datetime
+    update_at: datetime
+
+    def __eq__(self, other: object) -> bool:
+        """Check if two entities are equal based on their ID.
+
+        Args:
+            other (object): The other entity to compare with.
+
+        Returns:
+            bool: True if the entities are equal, False otherwise.
+        """
+        if not isinstance(other, BaseEntity):
+            return False
+        return self.id == other.id
