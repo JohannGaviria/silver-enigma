3e31d42ae6d5fbf3e9b8d8824f6575ce473f8be7
Johann Gaviria <johannmgr@gmail.com>
2026-04-22 15:26:39 -0500

    feat(api): introduce health check endpoint for service monitoring

--- src/main.py
+++ src/main.py
@@ -1,8 +1,11 @@
 """This module contains the main application code for the FastAPI application."""
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
+from fastapi.responses import JSONResponse
 
 from src.config import settings
+from src.shared.infrastructure.cache.redis_connection import RedisConnection
+from src.shared.infrastructure.database.database_engine import DatabaseEngine
 from src.shared.infrastructure.logging.structlog_configure_logging import (
     StructlogConfigureLogging,
 )
@@ -24,10 +27,48 @@ StructlogConfigureLogging.configure(debug=settings.DEBUG)
     summary="Root Endpoint",
     description="Returns a welcome message.",
 )
-async def root() -> dict:
+async def root() -> JSONResponse:
     """Root endpoint that returns a welcome message.
 
     Returns:
         dict: A dictionary containing a welcome message.
     """
-    return {"message": f"Welcome to {settings.APP_NAME}!"}
+    return JSONResponse(
+        content={"message": f"Welcome to the {settings.APP_NAME}!"},
+        status_code=status.HTTP_200_OK,
+    )
+
+
+@app.get(
+    path="/health",
+    tags=["System"],
+    summary="Health Check Endpoint",
+    description="Checks the operational status of the server, PostgreSQL, and Redis.",
+)
+async def health_check() -> JSONResponse:
+    """Health check endpoint that verifies the status of the server, PostgreSQL, and Redis.
+
+    Returns:
+        dict: A dictionary containing the health status of the services.
+    """
+    db_status = DatabaseEngine.health_check()
+    redis_status = RedisConnection.health_check()
+
+    if db_status and redis_status:
+        return JSONResponse(
+            content={
+                "status": "healthy",
+                "database": db_status,
+                "redis": redis_status,
+            },
+            status_code=status.HTTP_200_OK,
+        )
+
+    raise HTTPException(
+        status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
+        detail={
+            "status": "unhealthy",
+            "database": db_status,
+            "redis": redis_status,
+        },
+    )
