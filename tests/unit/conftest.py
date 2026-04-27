fddb70ff11e15c94dcf8ade48634613315241b15
Johann Gaviria <johannmgr@gmail.com>
2026-04-27 17:18:09 -0500

    test(auth): verify create first admin logic

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -1,6 +1,22 @@
+from unittest.mock import AsyncMock, Mock
+
 import pytest
+
+# ===============================
+# Modules: AUTH
+# ===============================
 
 
 @pytest.fixture()
 def password_hash() -> str:
     return "$2b$12$LQv3c1yqBWVHxkd0LHAkCOYz6TtxMQJqhN8/LewY5GyYJXRz.HV9K"
+
+
+@pytest.fixture()
+def user_repository_mock() -> AsyncMock:
+    return AsyncMock()
+
+
+@pytest.fixture()
+def password_hash_outbound_mock() -> Mock:
+    return Mock()
---
+++ tests/unit/modules/auth/application/use_cases/test_create_first_admin_use_case.py
@@ -0,0 +1,144 @@
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from faker import Faker
+
+from src.modules.auth.application.dtos.create_first_admin_dto import (
+    CreateFirstAdminCommand,
+)
+from src.modules.auth.application.use_cases.create_first_admin_use_case import (
+    CreateFirstAdminUseCase,
+)
+from src.modules.auth.domain.enums.user_role_enum import UserRoleEnum
+from src.modules.auth.domain.exceptions.auth_exception import (
+    AdminAlreadyExistsException,
+    InvalidEmailException,
+    InvalidNameException,
+    InvalidPlainPasswordException,
+)
+
+
+class TestCreateFirstAdminUseCase:
+    @pytest.mark.asyncio
+    async def test_should_a(
+        self,
+        faker: Faker,
+        user_repository_mock: AsyncMock,
+        password_hash_outbound_mock: Mock,
+        password_hash: str,
+    ) -> None:
+        user_repository_mock.exists_by_role.return_value = False
+        password_hash_outbound_mock.hash.return_value = password_hash
+
+        command = CreateFirstAdminCommand(
+            name=faker.name(), email=faker.email(), plain_password=faker.password()
+        )
+
+        user_repository_mock.save.side_effect = lambda user: user
+
+        use_case = CreateFirstAdminUseCase(
+            user_repository=user_repository_mock,
+            password_hash_outbound=password_hash_outbound_mock,
+        )
+
+        result = await use_case.execute(command)
+
+        assert result.name == command.name
+        assert result.email == command.email
+        assert result.role == UserRoleEnum.ADMIN
+
+    @pytest.mark.asyncio
+    async def test_should_b(
+        self,
+        faker: Faker,
+        user_repository_mock: AsyncMock,
+        password_hash_outbound_mock: Mock,
+        password_hash: str,
+    ) -> None:
+        user_repository_mock.exists_by_role.return_value = True
+        password_hash_outbound_mock.hash_return_value = password_hash
+
+        command = CreateFirstAdminCommand(
+            name=faker.name(), email=faker.email(), plain_password=faker.password()
+        )
+
+        use_case = CreateFirstAdminUseCase(
+            user_repository=user_repository_mock,
+            password_hash_outbound=password_hash_outbound_mock,
+        )
+
+        with pytest.raises(AdminAlreadyExistsException):
+            await use_case.execute(command)
+
+    @pytest.mark.asyncio
+    async def test_should_c(
+        self,
+        faker: Faker,
+        user_repository_mock: AsyncMock,
+        password_hash_outbound_mock: Mock,
+        password_hash: str,
+    ) -> None:
+        user_repository_mock.exists_by_role.return_value = False
+        password_hash_outbound_mock.hash_return_value = password_hash
+
+        command = CreateFirstAdminCommand(
+            name=f"{faker.name() * 5}",
+            email=faker.email(),
+            plain_password=faker.password(),
+        )
+
+        use_case = CreateFirstAdminUseCase(
+            user_repository=user_repository_mock,
+            password_hash_outbound=password_hash_outbound_mock,
+        )
+
+        with pytest.raises(InvalidNameException):
+            await use_case.execute(command)
+
+    @pytest.mark.asyncio
+    async def test_should_d(
+        self,
+        faker: Faker,
+        user_repository_mock: AsyncMock,
+        password_hash_outbound_mock: Mock,
+        password_hash: str,
+    ) -> None:
+        user_repository_mock.exists_by_role.return_value = False
+        password_hash_outbound_mock.hash_return_value = password_hash
+
+        command = CreateFirstAdminCommand(
+            name=faker.name(), email="invalid@email", plain_password=faker.password()
+        )
+
+        use_case = CreateFirstAdminUseCase(
+            user_repository=user_repository_mock,
+            password_hash_outbound=password_hash_outbound_mock,
+        )
+
+        with pytest.raises(InvalidEmailException):
+            await use_case.execute(command)
+
+    @pytest.mark.asyncio
+    async def test_should_e(
+        self,
+        faker: Faker,
+        user_repository_mock: AsyncMock,
+        password_hash_outbound_mock: Mock,
+        password_hash: str,
+    ) -> None:
+        user_repository_mock.exists_by_role.return_value = False
+        password_hash_outbound_mock.hash_return_value = password_hash
+
+        command = CreateFirstAdminCommand(
+            name=faker.name(),
+            email=faker.email(),
+            plain_password=faker.password(length=5),
+        )
+
+        use_case = CreateFirstAdminUseCase(
+            user_repository=user_repository_mock,
+            password_hash_outbound=password_hash_outbound_mock,
+        )
+
+        with pytest.raises(InvalidPlainPasswordException):
+            await use_case.execute(command)
