70a5744144ba05a1a17a99462151bded5222e7b4
Johann Gaviria <johannmgr@gmail.com>
2026-06-01 16:25:46 -0500

    test(products): add e2e tests for product creation and refactor exception handlers

--- src/main.py
+++ src/main.py
@@ -9,10 +9,10 @@ from src.modules.auth.presentation.api.exceptions.auth_exception_handler import 
     auth_exception_handlers,
 )
 from src.modules.auth.presentation.api.routes import auth_router
-from src.modules.products.presentation.api.routes import products_router
-from src.modules.products.presentation.exceptions.product_exception_handlers import (
+from src.modules.products.presentation.api.exceptions.product_exception_handlers import (
     product_exception_handlers,
 )
+from src.modules.products.presentation.api.routes import products_router
 from src.modules.warehouses.presentation.api.exceptions.warehouse_exception_handlers import (
     warehouse_exception_handlers,
 )
